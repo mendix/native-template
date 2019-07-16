2aafa8aab8b5540b22a63cfcd691d8eaa7539b75
Jeremiah Ogbomo <jeremiah.ogbomo@mendix.com>
2019-07-16 14:06:40 +0200

    Include dependencies and react-native patch file

---
+++ android/app/src/main/java/com/mendix/nativetemplate/MainActivity.java
@@ -0,0 +1,19 @@
+package com.mendix.nativetemplate;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.mendix.mendixnative.activity.MendixReactActivity;
+import com.mendix.mendixnative.config.AppUrl;
+import com.mendix.mendixnative.react.MendixApp;
+import com.mendix.mendixnative.react.MxConfiguration;
+
+public class MainActivity extends MendixReactActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        mendixApp = new MendixApp(AppUrl.getUrlFromResource(this),
+                MxConfiguration.WarningsFilter.none);
+
+        super.onCreate(savedInstanceState);
+    }
+}
