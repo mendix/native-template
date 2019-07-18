584298072cf5f3ef90cb0d202f6da9bba816a349
Stefanos Markidis <stmarkidis@gmail.com>
2019-07-18 10:54:16 +0200

    Restructure project and add Mendix native

---
+++ ios/AppDelegate.swift
@@ -0,0 +1,20 @@
+import Foundation
+import UIKit
+import MendixNative
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    guard let url = Bundle.main.object(forInfoDictionaryKey: "Runtime url") as? String, let runTimeUrl = AppUrl.forRuntime(url: url) else {
+      fatalError("Missing runtime url")
+    }
+    guard let bundleUrl = Bundle.main.url(forResource: "index.ios", withExtension: "bundle", subdirectory: "Bundle") else {
+      fatalError("Could not load index.ios.bundle")
+    }
+
+    ReactNative.instance.start(MendixApp(bundleUrl: bundleUrl, runtimeUrl: runTimeUrl))
+    return true
+  }
+}
