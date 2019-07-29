f4db77d811cd7f79b46a22dd6568b58d0c5cb29e
Stefanos Markidis <stmarkidis@gmail.com>
2019-07-29 13:44:39 +0200

    Extract resources for both platforms

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -1,1 +1,5 @@
+# Update project's dependencies
 npm i
+
+# Untar bundle resources
+tar xvzf android/res.tar.gz -C android/app/src/main/res && sudo find android/app/src/main/res/. -type d -exec chmod u+rwx {} \;
--- ios/appcenter-post-clone.sh
+++ ios/appcenter-post-clone.sh
@@ -4,3 +4,6 @@ brew install node
 
 # Update project's dependencies
 npm i && pod install --repo-update
+
+# Untar bundle resources
+tar xvzf ios/Bundle/assets.tar.gz -C ./ios/Bundle && sudo chmod u+rwx ios/Bundle/img
