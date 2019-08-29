aeb0bba34dd532f435c91f7a30dcd7b62807c8cd
Jeremiah Ogbomo <jeremiah.ogbomo@mendix.com>
2019-08-29 10:11:30 +0200

    Repurpose appcenter build scripts

--- ios/appcenter-pre-build.sh
+++
@@ -1,5 +0,0 @@
-echo "Stripping unwanted MendixNative (i386, x86_64) archs"
-LIB_PATH="./MendixNative/libMendix.a"
-lipo -remove x86_64 -output $LIB_PATH $LIB_PATH
-lipo -remove i386 -output $LIB_PATH $LIB_PATH
-lipo -info $LIB_PATH
