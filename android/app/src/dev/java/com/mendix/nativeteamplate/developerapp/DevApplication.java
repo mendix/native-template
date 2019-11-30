d150ecf6b758677639c051d0b4c7ea8d7f5180ba
Stefanos Markidis <stmarkidis@gmail.com>
2019-11-30 15:48:20 +0100

    Implement dev app flavor and basic interface

---
+++ android/app/src/dev/java/com/mendix/nativeteamplate/developerapp/DevApplication.java
@@ -0,0 +1,10 @@
+package com.mendix.nativeteamplate.developerapp;
+
+import com.mendix.nativetemplate.MainApplication;
+
+public class DevApplication extends MainApplication {
+    @Override
+    public boolean getUseDeveloperSupport() {
+        return true;
+    }
+}
