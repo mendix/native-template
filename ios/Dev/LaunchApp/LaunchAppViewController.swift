fb5a7852209a496dd363b354b220788ac3ca0915
Jeremiah Ogbomo <jeremiah.ogbomo@mendix.com>
2019-12-04 15:45:43 +0100

    Introduce QR Screen

--- ios/Dev/LaunchApp/LaunchAppViewController.swift
+++ ios/Dev/LaunchApp/LaunchAppViewController.swift
@@ -1,9 +1,11 @@
 import UIKit
 import MendixNative
 
-class LaunchAppViewController: UIViewController {
+class LaunchAppViewController: UIViewController, QRViewDelegate {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var clearDataSwitch: UISwitch!
+    @IBOutlet weak var qrView: QRView!
+    @IBOutlet weak var qrMaskView: QRMaskView!
 
     private var uiState: State = .idle {
         didSet {
@@ -14,6 +16,8 @@ class LaunchAppViewController: UIViewController {
     }
 
     func launchApp(_ url: String) {
+        uiState = .idle
+        qrView.stopScanning()
         AppPreferences.setAppUrl(url: url)
         AppPreferences.remoteDebugging(enable: false)
         AppPreferences.devMode(enable:  true)
@@ -26,41 +30,66 @@ class LaunchAppViewController: UIViewController {
 
     @IBAction func onTapGo(_ sender: Any) {
         if (uiState == .loading) {
-            return;
+            return
         }
 
         uiState = .loading
-        validateMendixUrl(textField.text ?? "") { (running) in
-            guard running, let url = self.textField.text, AppUrl.isValid(url: url) else {
+        let url = textField.text ?? ""
+        validateMendixUrl(url) { (running) in
+            guard running, AppUrl.isValid(url: url) else {
                 self.uiState = .invalidInput
                 return
             }
             self.textField.resignFirstResponder()
-            self.uiState = .idle
             self.launchApp(url)
         }
     }
 
+    func qrScanningSucceeded(_ str: String?) {
+        if uiState == .loading {
+            return
+        }
+
+        uiState = .loading
+        if let jsonString = str, let json = try? JSONSerialization.jsonObject(with: Data(jsonString.utf8), options: []) as? [String: Any], let parsedJson = json, let url = parsedJson["url"] as? String {
+            validateMendixUrl(url) { (running) in
+                guard running, AppUrl.isValid(url: url) else {
+                    self.uiState = .invalidInput
+                    return
+                }
+                self.launchApp(url)
+            }
+            return
+        }
+        uiState = .invalidQRCode
+    }
+
     override func viewDidLayoutSubviews() {
+        setupQrCodeMask(size: view.frame.size)
     }
 
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        setupQrCodeMask(size: size)
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         uiState = .idle
+        qrView.startScanning()
         textField.text = AppPreferences.getAppUrl()
         registerNotificationObservers()
     }
 
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
+        qrView.stopScanning()
         unregisterNotificationObservers()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        qrView.delegate = self
 
         textField.layer.cornerRadius = 30
         if let placeHolderText = textField.placeholder {
@@ -83,6 +112,8 @@ class LaunchAppViewController: UIViewController {
     }
 }
 
+private let MAX_MASK_SIZE = CGFloat(350)
+private let MASK_RELATIVE_SIZE = CGFloat(0.6)
 private let KEYBOARD_ANIMATION_DURATION_IN_S = 0.25
 private let KEYBOARD_SHOW_ANIMATION_DEBOUNCE_TIME_IN_S = 0.01
 
@@ -106,6 +137,13 @@ extension LaunchAppViewController {
         onCompletion(false)
     }
 
+    private func setupQrCodeMask(size: CGSize) {
+        let minDim = min(size.width, size.height)
+        var maskSize = minDim * MASK_RELATIVE_SIZE
+        maskSize = maskSize > MAX_MASK_SIZE ? MAX_MASK_SIZE : maskSize
+        qrMaskView.mask(rect: CGRect(x: (size.width - maskSize) / 2, y: (size.height - maskSize) / 2, width: maskSize, height: maskSize))
+    }
+
     private func registerNotificationObservers() {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: .UIKeyboardWillShow, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: .UIKeyboardWillHide, object: nil)
@@ -122,6 +160,8 @@ extension LaunchAppViewController {
             if let keyboardRect = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? CGRect {
                 let duration = notification.userInfo?[UIKeyboardAnimationDurationUserInfoKey] as? Double
                 self.view.frame.size.height = UIScreen.main.bounds.height - keyboardRect.height
+                self.qrView.stopScanning()
+                self.qrView.layer.opacity = 0
                 UIView.animate(withDuration: duration ?? KEYBOARD_ANIMATION_DURATION_IN_S, animations: {
                     self.view.layoutIfNeeded()
                 })
@@ -133,6 +173,8 @@ extension LaunchAppViewController {
     @objc func keyboardWillHide(_ notification: Notification) {
         let duration = notification.userInfo?[UIKeyboardAnimationDurationUserInfoKey] as? Double
         view.frame.size.height = UIScreen.main.bounds.height
+        self.qrView.startScanning()
+        self.qrView.layer.opacity = 1
         UIView.animate(withDuration: duration ?? KEYBOARD_ANIMATION_DURATION_IN_S, animations: {
             self.view.layoutIfNeeded()
         })
@@ -156,7 +198,7 @@ extension LaunchAppViewController {
         case .invalidInput:
             textField.layer.opacity = 1
             showErrorDialog(error: "The Mendix app may not be running at the moment or its an invalid URL.")
-            break;
+            break
         case .invalidQRCode:
             textField.layer.opacity = 1
             showErrorDialog(error: "This is not a Mendix QR code.")
