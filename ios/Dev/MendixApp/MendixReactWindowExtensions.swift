2809e81d01f412fbf1c10f687ab8b6ca69fd3077
Jeremiah Ogbomo <jeremiah.ogbomo@mendix.com>
2019-12-04 15:52:59 +0100

    Introduce ReleaseDevApp configuration

---
+++ ios/Dev/MendixApp/MendixReactWindowExtensions.swift
@@ -0,0 +1,13 @@
+import MendixNative
+
+extension MendixReactWindow {
+  open override var canBecomeFirstResponder: Bool {
+    return true
+  }
+
+  open override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
+    if (motion == .motionShake) {
+      ReactNative.instance.showAppMenu();
+    }
+  }
+}
