466612783d84e672958109aa4c45a0be25fc74ff
Jeremiah Ogbomo <jeremiah.ogbomo@mendix.com>
2019-12-09 13:57:56 +0100

    Include IS_DEV_APP flag to pre-build script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 INFO_PLIST=$APPCENTER_SOURCE_DIRECTORY/ios/$APPCENTER_XCODE_SCHEME/Info.plist
-if [[ -e "$INFO_PLIST" ]]; then
+if [[ -e "$INFO_PLIST" && $IS_DEV_APP == False ]]; then
     echo "Stripping unwanted MendixNative (i386, x86_64) archs"
     LIB_PATH=$APPCENTER_SOURCE_DIRECTORY/ios/MendixNative/libMendix.a
     lipo -remove x86_64 -output $LIB_PATH $LIB_PATH
@@ -15,7 +15,7 @@ if [[ -e "$INFO_PLIST" ]]; then
 fi
 
 CODE_PUSH_KEY_FILE=$APPCENTER_SOURCE_DIRECTORY/android/app/src/main/res/raw/code_push_key
-if [[ -e "$CODE_PUSH_KEY_FILE" ]]; then
+if [[ -e "$CODE_PUSH_KEY_FILE" && $IS_DEV_APP == False ]]; then
     echo "Updating Android code_push_key resource file with code push key"
     sed -i '' 's/.*/'$CODE_PUSH_KEY'/' $CODE_PUSH_KEY_FILE;
 
