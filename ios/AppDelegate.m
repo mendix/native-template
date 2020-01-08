04f429dc2dc7376356686c3e64c52b20f63ae0b1
Jeremiah Ogbomo <jeremiah.ogbomo@mendix.com>
2020-01-08 13:08:34 +0100

    Implementation details for firebase on iOS

--- ios/AppDelegate.m
+++ ios/AppDelegate.m
@@ -1,10 +1,18 @@
+#import <Firebase.h>
 #import "AppDelegate.h"
 #import "MendixNative/MendixNative.h"
 #import "IQKeyboardManager/IQKeyboardManager.h"
+#import "RNFirebase/RNFirebaseNotifications.h"
+#import "RNFirebase/RNFirebaseMessaging.h"
 
 @implementation AppDelegate
 
 - (BOOL) application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  if (self.useFirebase) {
+    [FIRApp configure];
+    [RNFirebaseNotifications configure];
+  }
+
   NSBundle *mainBundle = [NSBundle mainBundle];
   NSString *targetName = [mainBundle objectForInfoDictionaryKey:@"TargetName"] ?: @"";
 
@@ -23,6 +31,30 @@
   [ReactNative.instance start:[[MendixApp alloc] init:nil bundleUrl:bundleUrl runtimeUrl:runtimeUrl warningsFilter:none enableGestures:false clearDataAtLaunch:false reactLoadingStoryboard:nil]];
   
   return YES;
+}
+
+- (void) application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
+  if (self.useFirebase) {
+    [[RNFirebaseNotifications instance] didReceiveLocalNotification:notification];
+  }
+}
+
+- (void) application:(UIApplication *)application didReceiveRemoteNotification:(nonnull NSDictionary *)userInfo
+fetchCompletionHandler:(nonnull void (^)(UIBackgroundFetchResult))completionHandler{
+  if (self.useFirebase) {
+    [[RNFirebaseNotifications instance] didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
+  }
+}
+
+- (void) application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings {
+  if (self.useFirebase) {
+    [[RNFirebaseMessaging instance] didRegisterUserNotificationSettings:notificationSettings];
+  }
+}
+
+- (BOOL) useFirebase {
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"GoogleService-Info" ofType:@"plist"];
+    return [[NSFileManager defaultManager] fileExistsAtPath:path];
 }
 
 @end
