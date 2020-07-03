d94984a8e6a51ac693b528d10aa23ce0a260d929
Jeremiah Ogbomo <jeremiah.ogbomo@mendix.com>
2020-07-03 18:44:44 +0100

    Introduce splash screen dependency
    (cherry picked from commit b55eb5b158fd022734417a0aad1a41f768f0fc05)

--- react-native.config.js
+++ react-native.config.js
@@ -17,6 +17,12 @@ module.exports = {
                 // disable only on Android as we require some conditional setup
                 android: null,
             },
+        },
+        'react-native-splash-screen': {
+            platforms: {
+                android: null,
+                ios: null,
+            },
         }
     },
 };
