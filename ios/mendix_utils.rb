df32f5f6aca34fdf2f83507710c1773a5e110ec7
Jeremiah Ogbomo <jeremiah.ogbomo@mendix.com>
2020-07-22 10:48:05 +0100

    Format ruby script with rufo
    (cherry picked from commit a47d87250e560a3cd4dfa558bbc741f2b11797ea)

--- ios/mendix_utils.rb
+++ ios/mendix_utils.rb
@@ -1,66 +1,67 @@
-require 'json'
+require "json"
 
-def generate_pod_dependencies!
-	modules = get_react_native_config!["dependencies"]
-	config = get_project_config!
-	config["dependencies"].each do |name, options|
-		modules.include?(name) && include_pods!(name, options["ios"]["pods"])
-	end
+def generate_pod_dependencies
+  modules = get_react_native_config["dependencies"]
+  config = get_project_config
+  config["dependencies"].each do |name, options|
+    include_pods(name, options["ios"]["pods"]) if modules.include?(name)
+  end
 end
 
-def generate_mendix_delegate!
-	imports = []
-	hooks = {
-		didFinishLaunchingWithOptions: [],
-		didReceiveLocalNotification: [],
-		didReceiveRemoteNotification: [],
-		didRegisterUserNotificationSettings: [],
-		openURL: [],
-	}
+def generate_mendix_delegate
+  imports = []
+  hooks = {
+    didFinishLaunchingWithOptions: [],
+    didReceiveLocalNotification: [],
+    didReceiveRemoteNotification: [],
+    didRegisterUserNotificationSettings: [],
+    openURL: [],
+  }
 
-	capabilities = []
-	config = get_project_config!
-	get_project_capabilities!.select { |_, value| value == true }.each do |name, _|
-		capability = config["capabilities"][name.to_s]
-		if capability.nil?
-				Pod::UI.warn "'#{name.to_s}' is not a valid Mendix capability. This file should not be manipulated without guidance."
+  capabilities = []
+  config = get_project_config
+  get_project_capabilities.select { |_, value| value == true }.each do |name, _|
+    capability = config["capabilities"][name.to_s]
+    if capability.nil?
+      Pod::UI.warn "'#{name.to_s}' is not a valid Mendix capability. This file should not be manipulated without guidance."
+      next
     end
-    next if capability.nil?
 
-		Pod::UI.notice "'#{name.to_s}' capability was enabled for this project."
-		capabilities << capability["ios"]
-	end
+    Pod::UI.notice "'#{name.to_s}' capability was enabled for this project."
+    capabilities << capability["ios"]
+  end
 
-	capabilities.each do |options|
-		!options['imports'].nil? && imports << options['imports'].map { |import| "#import #{import}" }
+  capabilities.each do |options|
+    imports << options["imports"].map { |import| "#import #{import}" } if !options["imports"].nil?
 
-		!options['hooks'].nil? && hooks.each do |name, hook|
-			!options['hooks'][name.to_s].nil? && hook << options['hooks'][name.to_s].map { |line| "#{line};" }
-		end
-	end
+    if !options["hooks"].nil?
+      hooks.each do |name, hook|
+        hook << options["hooks"][name.to_s].map { |line| "#{line};" } if !options["hooks"][name.to_s].nil?
+      end
+    end
+  end
 
-	open('MendixAppDelegate.m', 'w') do |file|
-		file << replace_template_with_values!(
-			imports.flatten.uniq.join("\n"),
-			hooks[:didFinishLaunchingWithOptions].flatten.uniq.join("\n"),
-			hooks[:didReceiveLocalNotification].flatten.uniq.join("\n"),
-			hooks[:didReceiveRemoteNotification].flatten.uniq.join("\n"),
-			hooks[:didRegisterUserNotificationSettings].flatten.uniq.join("\n"),
-			hooks[:openURL].flatten.uniq.join("\n")
-		)
-	end
+  File.open("MendixAppDelegate.m", "w") do |file|
+    file << replace_template_with_values(
+      imports.flatten.uniq.join("\n"),
+      hooks[:didFinishLaunchingWithOptions].flatten.uniq.join("\n"),
+      hooks[:didReceiveLocalNotification].flatten.uniq.join("\n"),
+      hooks[:didReceiveRemoteNotification].flatten.uniq.join("\n"),
+      hooks[:didRegisterUserNotificationSettings].flatten.uniq.join("\n"),
+      hooks[:openURL].flatten.uniq.join("\n")
+    )
+  end
 end
 
-def replace_template_with_values!(
-	imports = "", 
-	didFinishLaunchingWithOptions = "",
-	didReceiveLocalNotification = "",
-	didReceiveRemoteNotification = "",
-	didRegisterUserNotificationSettings = "",
-	openURL = ""
+def replace_template_with_values(
+  imports,
+  didFinishLaunchingWithOptions,
+  didReceiveLocalNotification,
+  didReceiveRemoteNotification,
+  didRegisterUserNotificationSettings,
+  openURL
 )
-	return %(// DO NOT EDIT BY HAND. THIS FILE IS AUTO-GENERATED
-
+  %(// DO NOT EDIT BY HAND. THIS FILE IS AUTO-GENERATED
 #import <Foundation/Foundation.h>
 #import "MendixAppDelegate.h"
 #{imports}
@@ -92,37 +93,37 @@ fetchCompletionHandler:(nonnull void (^)(UIBackgroundFetchResult))completionHand
 	)
 end
 
-def get_project_config!
-	return JSON.parse(File.read(File.join(__dir__, '..', 'config.json')))
+def get_project_config
+  JSON.parse(File.read(File.join(__dir__, "..", "config.json")))
 end
 
-def get_project_capabilities!
-	return JSON.parse(File.read(File.join(__dir__, '..', 'capabilities.json')))
+def get_project_capabilities
+  JSON.parse(File.read(File.join(__dir__, "..", "capabilities.json")))
 end
 
 # Source @react-native-community/cli-platform-ios/native_modules
-def get_react_native_config!
-	cli_command = "try {console.log(require('@react-native-community/cli').bin);} catch (e) {console.log(require('react-native/cli').bin);}"
-	cli_result = Pod::Executable.execute_command("node", ["-e", cli_command], true).strip
+def get_react_native_config
+  cli_command = "try {console.log(require('@react-native-community/cli').bin);} catch (e) {console.log(require('react-native/cli').bin);}"
+  cli_result = Pod::Executable.execute_command("node", ["-e", cli_command], true).strip
 
-	json = []
-	IO.popen(["node", cli_result, "config"]) do |data|
-			while line = data.gets
-			json << line
-			end
-	end
+  json = []
+  IO.popen(["node", cli_result, "config"]) do |data|
+    while line = data.gets
+      json << line
+    end
+  end
 
-	return JSON.parse(json.join("\n"))
+  JSON.parse(json.join("\n"))
 end
 
-def include_pods!(name, pods)
-	pods.each do |name, pod|
-		if pod["path"] != nil && !pod["path"].empty?
-			pod name, :path => "../node_modules/#{pod['path']}"
-		elsif pod["version"] != nil && !pod["version"].empty?
-			pod name, pod["version"]
-		else
-			pod name
-		end
-	end
+def include_pods(name, pods)
+  pods.each do |name, pod|
+    if pod["path"] != nil && !pod["path"].empty?
+      pod name, :path => "../node_modules/#{pod["path"]}"
+    elsif pod["version"] != nil && !pod["version"].empty?
+      pod name, pod["version"]
+    else
+      pod name
+    end
+  end
 end
