e55524b097794d1d7ad14c999b9670d66c5250c0
smarki <stmarkidis@gmail.com>
2020-07-23 14:02:32 +0200

    Support dynamic capability linking in android

--- android/app/src/main/java/com/mendix/nativetemplate/MainApplication.java
+++ android/app/src/main/java/com/mendix/nativetemplate/MainApplication.java
@@ -12,12 +12,7 @@ import com.microsoft.codepush.react.CodePush;
 
 import org.devio.rn.splashscreen.SplashScreen;
 
-import java.util.Arrays;
 import java.util.List;
-
-import io.invertase.firebase.RNFirebasePackage;
-import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
-import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 
 public class MainApplication extends MendixReactApplication {
     @Override
@@ -28,18 +23,11 @@ public class MainApplication extends MendixReactApplication {
     @Override
     public List<ReactPackage> getPackages() {
         List<ReactPackage> packages = new PackageList(this).getPackages();
+        packages.addAll(new MendixPackageList(this).getPackages());
 
         // Packages that cannot be autolinked yet can be added manually here, for example:
         // packages.add(new MyReactNativePackage());
         packages.add(new CodePush(getCodePushKey(), getApplicationContext(), BuildConfig.DEBUG));
-
-        if (BuildConfig.USE_FIREBASE) {
-            packages.addAll(Arrays.asList(
-                    new RNFirebaseMessagingPackage(),
-                    new RNFirebaseNotificationsPackage(),
-                    new RNFirebasePackage()
-            ));
-        }
 
         return packages;
     }
