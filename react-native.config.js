b2b9fd499fb4314e69e7780ec570265de0b777e2
Jeremiah Ogbomo <jeremiah.ogbomo@mendix.com>
2020-07-23 13:41:27 +0100

    Refactor lingering camera dependency manual linking
    (cherry picked from commit 70a088eee9441f854d22476fb3ba6368ae5e0c0b)

--- react-native.config.js
+++ react-native.config.js
@@ -1,12 +1,6 @@
 module.exports = {
     dependencies: {
-        "react-native-camera": {
-            platforms: {
-                // disable only on iOS as we require some conditional setup
-                ios: null,
-            },
-        },
-        "react-native-code-push": {
+        'react-native-code-push': {
             platforms: {
                 // disable only on Android as we require some conditional setup
                 android: null,
