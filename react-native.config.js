7fa385984ac0180693800677aa59a7afb2907b48
smarki <stmarkidis@gmail.com>
2020-07-23 14:02:32 +0200

    Support dynamic capability linking in android

--- react-native.config.js
+++ react-native.config.js
@@ -1,22 +1,16 @@
 module.exports = {
-    dependencies: {
-        'react-native-firebase': {
-            platforms: {
-                // disable only on Android as we require some conditional setup
-                android: null,
-            },
-        },
-        'react-native-camera': {
-            platforms: {
-                // disable only on iOS as we require some conditional setup
-                ios: null,
-            },
-        },
-        'react-native-code-push': {
-            platforms: {
-                // disable only on Android as we require some conditional setup
-                android: null,
-            },
-        }
+  dependencies: {
+    "react-native-camera": {
+      platforms: {
+        // disable only on iOS as we require some conditional setup
+        ios: null,
+      },
     },
+    "react-native-code-push": {
+      platforms: {
+        // disable only on Android as we require some conditional setup
+        android: null,
+      },
+    },
+  },
 };
