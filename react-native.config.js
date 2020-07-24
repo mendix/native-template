d01085d34339e24387c99fb13cf22536f00e6306
smarki <stmarkidis@gmail.com>
2020-07-24 12:09:06 +0100

    Revert spacing
    (cherry picked from commit df8e41e6b932e9ba68a189291fd042d1fd16c352)

--- react-native.config.js
+++ react-native.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  dependencies: {
-    "react-native-camera": {
-      platforms: {
-        // disable only on iOS as we require some conditional setup
-        ios: null,
-      },
+    dependencies: {
+        "react-native-camera": {
+            platforms: {
+                // disable only on iOS as we require some conditional setup
+                ios: null,
+            },
+        },
+        "react-native-code-push": {
+            platforms: {
+                // disable only on Android as we require some conditional setup
+                android: null,
+            },
+        }
     },
-    "react-native-code-push": {
-      platforms: {
-        // disable only on Android as we require some conditional setup
-        android: null,
-      },
-    }
-  },
 };
