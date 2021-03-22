a1a0c2edb9a56ae37976ef7cf663eb015b3e47da
Jeremiah Ogbomo <jeremiah.ogbomo@mendix.com>
2021-03-22 10:29:09 +0100

    Introduce github action to update releases list on every release

---
+++ .github/update_releases_list.js
@@ -0,0 +1,24 @@
+const { writeFileSync } = require("fs");
+
+const relativeReleasesListPath = ".mx/releases_list.json";
+
+module.exports = async ({ github }) => {
+    const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+    const response = await github.repos.listReleases({ owner, repo });
+    const releases = response.data.map((release) => {
+        return {
+            id: release.id,
+            tag_name: release.tag_name,
+            name: release.name,
+            draft: release.draft,
+            prerelease: release.prerelease,
+            body: release.body,
+            created_at: release.created_at,
+            published_at: release.published_at,
+            tarball_url: release.tarball_url,
+            zipball_url: release.zipball_url,
+        }
+    });
+
+    writeFileSync(`${process.env.GITHUB_WORKSPACE}/${relativeReleasesListPath}`, `${JSON.stringify(releases, null, 4)}\n`)
+}
