054c2314c84c7d630fa0002d8087a7ab96560da0
smarki <stmarkidis@gmail.com>
2021-03-31 19:49:38 +0200

    Do not fail lipo step

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 
 if [[ $SUPPORTS_MOBILE_TOOLKIT == True ]]; then
     echo "Configuring template with mobile toolkit"
@@ -10,12 +9,12 @@ INFO_PLIST=$APPCENTER_SOURCE_DIRECTORY/ios/$APPCENTER_XCODE_SCHEME/Info.plist
 if [[ -e "$INFO_PLIST" && $IS_DEV_APP == False ]]; then
     echo "Stripping unwanted MendixNative (i386, x86_64) archs"
     LIB_PATH=$APPCENTER_SOURCE_DIRECTORY/ios/MendixNative/libMendix.a
-    lipo -remove x86_64 -output $LIB_PATH $LIB_PATH
-    lipo -remove i386 -output $LIB_PATH $LIB_PATH
-    lipo -info $LIB_PATH
+    lipo -remove x86_64 -output $LIB_PATH $LIB_PATH || true
+    lipo -remove i386 -output $LIB_PATH $LIB_PATH || true
+    lipo -info $LIB_PATH || true
 
     echo "Updating Info.plist with code push key"
-    plutil -replace "CodePushKey" -string $CODE_PUSH_KEY $INFO_PLIST
+    plutil -replace "CodePushKey" -string $CODE_PUSH_KEY $INFO_PLIST || true
 
     cat $INFO_PLIST
 fi
