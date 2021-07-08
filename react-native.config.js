64bf28d583aea3df96e4436b8285707d58fa0df5
Jeremiah Ogbomo <jeremiah.ogbomo@mendix.com>
2021-07-08 10:38:54 +0200

    Implement appCenterOTA capability setup configuration

--- react-native.config.js
+++ react-native.config.js
@@ -10,6 +10,8 @@ module.exports = {
             platforms: {
                 // disable only on Android as we require some conditional setup
                 android: null,
+                // would be linked only when the capability is enabled
+                ios: null,
             },
         },
         'react-native-splash-screen': {
