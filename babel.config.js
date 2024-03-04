d48b7e78f5724138183104115ae0e7267587b7a9
Pavel Hryn <pavel.hryn@mendix.com>
2024-03-04 14:30:42 +0100

    fix: update reanimated patch and gradle version

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: ['react-native-reanimated/plugin', {
+    relativeSourceLocation: true
+  }],
 };
