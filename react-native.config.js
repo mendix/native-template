887c20f58631513fbf02ee44ca048f739e7d640b
Uraz Akgültan <uraz@akgultan.com>
2024-06-21 13:39:25 +0200

    Downgrade RN video

--- react-native.config.js
+++ react-native.config.js
@@ -1,10 +1,17 @@
 module.exports = {
-    dependencies: {
-        'react-native-splash-screen': {
-            platforms: {
-                android: null,
-                ios: null,
-            },
-        },
+  dependencies: {
+    "react-native-splash-screen": {
+      platforms: {
+        android: null,
+        ios: null,
+      },
     },
+  },
+  "react-native-video": {
+    platforms: {
+      android: {
+        sourceDir: "../node_modules/react-native-video/android-exoplayer",
+      },
+    },
+  },
 };
