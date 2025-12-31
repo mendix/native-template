62d984a9005f41bb4c35fdb048fb057dd84c5d5d
Yogendra Shelke <25844542+YogendraShelke@users.noreply.github.com>
2025-12-31 21:53:15 +0530

    chore: enhance iOS session cookie management and update dependencies

--- ios/AppDelegate.swift
+++ ios/AppDelegate.swift
@@ -8,6 +8,7 @@ class AppDelegate: ReactAppProvider {
   
   override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     
+    SessionCookieStore.restore()
     MendixAppDelegate.application(application, didFinishLaunchingWithOptions: launchOptions)
     setupApp(application: application, launchOptions: launchOptions)
     changeRoot(to: UIViewController())
@@ -54,5 +55,13 @@ class AppDelegate: ReactAppProvider {
   
   func getWarningFilterValue() -> WarningsFilter {
     return .none
+  }
+  
+  override func applicationWillTerminate(_ application: UIApplication) {
+    SessionCookieStore.persist()
+  }
+  
+  override func applicationDidEnterBackground(_ application: UIApplication) {
+    SessionCookieStore.persist()
   }
 }
--- ios/Dev/AppDelegate.swift
+++ ios/Dev/AppDelegate.swift
@@ -9,6 +9,7 @@ class AppDelegate: ReactAppProvider {
   
   override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     
+    SessionCookieStore.restore()
     MendixAppDelegate.application(application, didFinishLaunchingWithOptions: launchOptions)
     setupApp(application: application, launchOptions: launchOptions)
     
@@ -66,5 +67,13 @@ class AppDelegate: ReactAppProvider {
     ReactNative.shared.start()
     
     return handled
+  }
+  
+  override func applicationWillTerminate(_ application: UIApplication) {
+    SessionCookieStore.persist()
+  }
+  
+  override func applicationDidEnterBackground(_ application: UIApplication) {
+    SessionCookieStore.persist()
   }
 }
